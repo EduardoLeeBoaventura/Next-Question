3f0486e2e143d74fe681addbd79739bc25857360
Lee <eduardoleema06@gmail.com>
2024-10-15 19:54:00 -0300

    Uso da funcionalidade de cadastro

---
+++ api/get/perguntas/consulta_perguntas.php
@@ -0,0 +1,37 @@
+<?php
+  require_once $_SERVER['DOCUMENT_ROOT'] . DIRECTORY_SEPARATOR . "src" . DIRECTORY_SEPARATOR . "resources" . DIRECTORY_SEPARATOR . "system_functions.php";
+
+  use System\Controller\Pergunta;
+
+  $perguntas_handler = new Pergunta();
+  
+  if(!empty($_GET)){
+    $ref = @$_GET['ref'];
+
+    $conditions = [
+      "ref = '$ref'"
+    ];
+    
+    $perguntas = $perguntas_handler->listar($conditions);
+
+    if($perguntas !== false && arrayLength($perguntas) > 0){
+        $response = [
+            "response" => $perguntas,
+            "status" => true,
+            "status_msg" => "Pergunta(s) encontradas."
+        ];
+    } else {
+        $response = [
+            "response" => [],
+            "status" => false,
+            "status_msg" => "Nenhuma pergunta(s) encontrada."
+        ];
+    }
+  } else {
+    $response = [
+        "response" => [],
+        "status" => false,
+        "status_msg" => "Dados incorretos ou insuficientes"
+    ];
+  }
+  echo json_encode($response);
